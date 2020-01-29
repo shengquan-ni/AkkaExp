f834a2972f0fd367afeddeb2e173afc82dbcc4fa
Shengquan Ni <Hiseen@github.noreply.com>
2020-01-28 18:50:38 -0800

    Update ThreadState.scala

--- src/main/scala/Engine/Common/ThreadState.scala
+++ src/main/scala/Engine/Common/ThreadState.scala
@@ -2,9 +2,9 @@ package Engine.Common
 
 
 object ThreadState extends Enumeration {
-  val Idle,
-  Running,
-  Paused,
-  LocalBreakpointTriggered,
-  Completed = Value
+  val Idle = Value("Idle")
+  val Running = Value("Running")
+  val Paused = Value("Paused")
+  val LocalBreakpointTriggered = Value("LocalBreakpointTriggered")
+  val Completed = Value("Completed")
 }
