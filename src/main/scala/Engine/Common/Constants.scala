4c5f1c6d4bd475f4b31a8b44086cb223baa3a1f1
Shengquan Ni <Hiseen@github.noreply.com>
2020-01-30 11:48:41 -0800

    Update Constants.scala

--- src/main/scala/Engine/Common/Constants.scala
+++ src/main/scala/Engine/Common/Constants.scala
@@ -3,6 +3,6 @@ package Engine.Common
 object Constants {
   val defaultBatchSize = 400
   val remoteHDFSPath = "hdfs://10.138.0.2:8020"
-  var defaultNumWorkers = 0
-  var dataset = 0
+  @volatile var defaultNumWorkers = 0
+  @volatile var dataset = 0
 }
