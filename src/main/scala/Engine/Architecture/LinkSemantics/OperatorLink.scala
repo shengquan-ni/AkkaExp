bc77774c35b5637349ad27ddf537f5973e7b523c
Shengquan Ni <Hiseen@github.noreply.com>
2020-01-30 00:30:11 -0800

    Update OperatorLink.scala

--- src/main/scala/Engine/Architecture/LinkSemantics/OperatorLink.scala
+++ src/main/scala/Engine/Architecture/LinkSemantics/OperatorLink.scala
@@ -27,7 +27,7 @@ class OperatorLink(val from: (OperatorMetadata,ActorRef), val to:(OperatorMetada
       //TODO: use type matching to generate a 'smarter' strategy based on the operators
       if(to._1.requiredShuffle){
         linkStrategy = new HashBasedShuffle(sender,receiver,Constants.defaultBatchSize,to._1.getShuffleHashFunction(sender.tag))
-      }else if(to._1.isInstanceOf[SimpleSinkOperatorMetadata] || to._1.isInstanceOf[SortMetadata]){
+      }else if(to._1.isInstanceOf[SimpleSinkOperatorMetadata] || to._1.isInstanceOf[SortMetadata[String]]){
         linkStrategy = new AllToOne(sender,receiver,Constants.defaultBatchSize)
       }
       else if(sender.layer.length == receiver.layer.length){
