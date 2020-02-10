9e7fe76229c4e9255b7ddfa644225c4a4a85a50a
Shengquan Ni <Hiseen@github.noreply.com>
2020-02-09 21:02:24 -0800

    Update Constants.scala

--- src/main/scala/Engine/Common/Constants.scala
+++ src/main/scala/Engine/Common/Constants.scala
@@ -5,7 +5,7 @@ import scala.concurrent.duration._
 object Constants {
   val defaultBatchSize = 400
   val remoteHDFSPath = "hdfs://10.138.0.2:8020"
-  val remoteHDFSIP = "34.83.111.228"
+  val remoteHDFSIP = "35.203.161.159"
   var defaultNumWorkers = 0
   var dataset = 0
   var masterNodeAddr:String = null
