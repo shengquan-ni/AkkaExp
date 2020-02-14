a3ad7bdb7e3d67b60cfa02500b6ca5b15d4dd727
Shengquan Ni <Hiseen@github.noreply.com>
2020-02-13 22:50:21 -0800

    upsd

--- src/main/scala/Engine/Architecture/SendSemantics/Routees/ActorRoutee.scala
+++ src/main/scala/Engine/Architecture/SendSemantics/Routees/ActorRoutee.scala
@@ -4,24 +4,45 @@ import Engine.Common.AmberMessage.ControlMessage.{Pause, Resume}
 import Engine.Common.AmberMessage.WorkerMessage.{DataMessage, EndSending}
 import akka.actor.{Actor, ActorContext, ActorRef, PoisonPill, Props}
 
+import scala.collection.mutable.ArrayBuffer
+
 abstract class ActorRoutee(receiver: ActorRef) extends BaseRoutee(receiver) {
 
   var senderActor:ActorRef = _
+  val stash = new ArrayBuffer[Any]
+  var isPaused = false
 
   override def pause(): Unit = {
+    isPaused = true
     senderActor ! Pause
   }
 
   override def resume()(implicit sender: ActorRef): Unit = {
     senderActor ! Resume
+    isPaused = false
+    for(i <- stash){
+      i match{
+        case d:DataMessage => senderActor ! d
+        case e:EndSending => senderActor ! e
+      }
+    }
+    stash.clear()
   }
 
   override def schedule(msg: DataMessage)(implicit sender: ActorRef): Unit = {
-    senderActor ! msg
+    if(isPaused){
+      stash.append(msg)
+    }else {
+      senderActor ! msg
+    }
   }
 
   override def schedule(msg: EndSending)(implicit sender: ActorRef): Unit = {
-    senderActor ! msg
+    if(isPaused){
+      stash.append(msg)
+    }else {
+      senderActor ! msg
+    }
   }
 
   override def dispose(): Unit = {
