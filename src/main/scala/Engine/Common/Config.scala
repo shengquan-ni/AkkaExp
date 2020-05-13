da4a899f7fa882890a5e733294f60b8a4029c688
Avinash Kumar <avinash0161@yahoo.com>
2020-05-13 13:55:49 -0700

    dev

--- src/main/scala/Engine/Common/Config.scala
+++ src/main/scala/Engine/Common/Config.scala
@@ -5,19 +5,23 @@ object Config {
   /**
    * The following is used to connect a socket at beginning to know own IP
    */
-  val thirdPartyIP: String = "128.195.52.129"
-  val thirdPartyPort:Int = 9870
-
+  // val thirdPartyIP: String = "128.195.52.129"
+  // val thirdPartyPort:Int = 9870
+  val thirdPartyIP: String = "172.31.33.27"
+  val thirdPartyPort:Int = 22
   /**
    * Put HDFS connection details below.
    */
-  val remoteHDFSPath = "hdfs://128.195.52.129:9871"
-  val remoteHDFSIP = "128.195.52.129"
-  var dataset = 20;
+//  val remoteHDFSPath = "hdfs://128.195.52.129:9871"
+//  val remoteHDFSIP = "128.195.52.129"
+  val remoteHDFSPath = "hdfs://ec2-18-216-84-85.us-east-2.compute.amazonaws.com:9871"
+  val remoteHDFSIP = "ec2-18-216-84-85.us-east-2.compute.amazonaws.com"
+  var dataset = 20
 
   /**
    * Automate number of workers per operator and data to be processed
    */
-  var numWorkerPerNode = 12
+  // var numWorkerPerNode = 12
+  var numWorkerPerNode = 4
   var dataVolumePerNode = 0
 }
