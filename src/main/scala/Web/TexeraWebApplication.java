f945ba8a45a2f766aa9aa4300f7849b43171aa4c
zuozhiw <zuozhiw@gmail.com>
2020-06-19 02:52:29 -0700

    add basic web application

--- src/main/scala/Web/TexeraWebApplication.java
+++ src/main/scala/Web/TexeraWebApplication.java
@@ -1,4 +1,20 @@
 package Web;
 
-public class TexeraWebApplication {
+import com.github.dirkraft.dropwizard.fileassets.FileAssetsBundle;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class TexeraWebApplication extends Application<TexeraWebConfiguration> {
+
+    @Override
+    public void initialize(Bootstrap<TexeraWebConfiguration> bootstrap) {
+        // serve static frontend GUI files
+        bootstrap.addBundle(new FileAssetsBundle("./new-gui/dist/", "/", "index.html"));
+    }
+
+    @Override
+    public void run(TexeraWebConfiguration configuration, Environment environment) throws Exception {
+
+    }
 }
---
+++ src/main/scala/Web/TexeraWebConfiguration.java
@@ -0,0 +1,6 @@
+package Web;
+
+import io.dropwizard.Configuration;
+
+public class TexeraWebConfiguration extends Configuration {
+}
