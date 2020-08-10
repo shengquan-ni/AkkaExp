36426b9ed53c7a129a0f92cff0c21f4a646893f2
zuozhiw <zuozhiw@gmail.com>
2020-08-09 17:15:18 -0700

    fix

--- src/main/scala/Engine/Operators/Scan/HDFSFileScan/HDFSFileScanTupleProducer.java
+++ src/main/scala/Engine/Operators/Scan/HDFSFileScan/HDFSFileScanTupleProducer.java
@@ -45,7 +45,7 @@ public class HDFSFileScanTupleProducer implements TupleProducer{
         //FileSystem fs = FileSystem.get(new URI(host),new Configuration());
         //FSDataInputStream stream = fs.open(new Path(hdfsPath));
         //stream.seek(startOffset);
-        URL url = new URL("http://"+host)+":9870/webhdfs/v1"+hdfsPath+"?op=OPEN&offset="+startOffset);
+        URL url = new URL("http://"+ host+":9870/webhdfs/v1"+hdfsPath+"?op=OPEN&offset="+startOffset);
         InputStream stream = url.openStream();
         reader = new BufferedBlockReader(stream,endOffset-startOffset,separator,indicesToKeep);
         if(startOffset > 0)
